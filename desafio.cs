aa82ff63da719da923b1ec0e2d128b1e9bfcbba6
Pedro Cyrino <pedroxcani@gmail.com>
2023-09-17 22:33:12 -0300

    desafio.cs

---
+++ desafio.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int numeroInicio = 0;
+        int numeroSequencial = 1;
+        
+        for (int i = 0; i < 100; i++)
+        {
+            int fibonachi = numeroInicio + numeroSequencial;
+            numeroInicio = numeroSequencial;
+            numeroSequencial = fibonachi;
+            Console.WriteLine(numeroInicio);
+        }
+    }
+}
